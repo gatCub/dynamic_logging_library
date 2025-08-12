3730a8c7a5f7ccee95dfc5c35165b4af232b9d39
Latypov <ruslanlatypov14@gmail.com>
2025-08-12 17:04:37 +0500

    Создания перечесления со уровнями логировани. Написания логики конструктора и деструктора.

--- scr/logger.cpp
+++ scr/logger.cpp
@@ -1,11 +1,11 @@
 #include "logger.h"
 
-logger::logger()
-{
-
+logger::logger(const std::string& filename, LogLevel defaultLogLevel): currentLevel_(defaultLogLevel) {
+    logFile_.open(filename, std::ios::app);
+    if (!logFile_.is_open()) throw std::runtime_error("Ошибка открытия файла логирования: " + filename);
 }
 
-logger::~logger()
-{
-
+logger::~logger() {
+    // по сути поток это RAII-Обьект и сам будет корректно закрываться но для полноты можно проверить 
+    if (logFile_.is_open()) logFile_.close();
 }
--- scr/logger.h
+++ scr/logger.h
@@ -3,13 +3,33 @@
 
 #pragma once
 
-class logger
+#include <string>
+#include <fstream>
+
+/*Уровни логирования: 
+    Info - Информационное сообщение.
+    Warning - Предупреждающие соотбщение (не критическое).
+    Error - критическая ошибка
+*/
+enum class LogLevel {
+    Info = 0,
+    Warning = 1,
+    Error = 2
+
+};
+
+struct logger
 {
-public:
-    logger();
+    logger(const std::string&, LogLevel);
     ~logger();
 
+    void log(const std::string&, LogLevel = LogLevel::Info);
+    void setLogLevel(LogLevel);
+
+
 private:
+    std::ofstream logFile_;
+    LogLevel currentLevel_;
 
 };
 
