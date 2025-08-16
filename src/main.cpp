a4483c309a56917b37ef4713a627909a2ebf474a
Latypov <ruslanlatypov14@gmail.com>
2025-08-16 18:45:27 +0500

    Реализация потокобезопасной очереди. Переработка проверок уровней логирования. Реализация работы логера которая запускается в отдельном потоке (сама логика работы в потоке пока просто заглушка функция - workerThread)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include <string>
+#include <string_view>
+#include <queue>
+#include <optional>
+
+#include <mutex>
+#include <atomic>
+#include <thread>
+#include <condition_variable>
 
 #include "logger.h"
 
@@ -8,8 +16,57 @@
 #endif
 
 void printHelp() {
+    //не забыть реализовать
     std::cout << "Implement it later..." << std::endl;
 }
+
+struct LogTask {
+    std::string message;
+    LogLevel level;
+};
+
+template<typename T>
+struct ThreadSafeQueue {
+    void push(const T& value) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.push(value);
+        cond_var_.notify_one();
+    }
+
+    bool pop(T& value) {
+        std::unique_lock<std::mutex> lock (mutex_);
+        cond_var_.wait(lock, [this] {return !queue_.empty() || stop_; });
+
+        if (stop_ && queue_.empty()) {
+            return false;
+        }
+
+        value = queue_.front();
+        queue_.pop();
+        return true;
+    }
+
+    void stop() {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            stop_ = true;
+        }
+
+        cond_var_.notify_all();
+    }
+
+private:
+    std::queue<T> queue_;
+    std::mutex mutex_;
+    std::condition_variable cond_var_;
+    std::atomic<bool> stop_ { false };    
+};
+
+//
+void workerThread(ThreadSafeQueue<LogTask>& queue, Logger& logger) {
+    //реализовать логику
+}
+
 
 
 int main(int argc, char** argv) {
@@ -21,33 +78,67 @@ int main(int argc, char** argv) {
 
     if( argc != 3 ) {
         std::cerr << "The program works as follows: " 
-                  << argv[0] << " <logging_file> <standard_logging_level>\n" 
+                  << argv[0] << " <logging_file> <defualt_logging_level>\n\n" 
                   << "for more information: --help" << std::endl;
 
         return 1;
    }
    
-
    std::string logfile { argv[1] };
    LogLevel defualtLevel;
-   
 
-   if (auto levelInt { std::stoi(argv[2]) }; levelInt < 0 || levelInt > 2 ) {
-        std::cerr << "Invalid default level. Use 0-2" << std::endl;
-        return 1;
-   } else {
-        defualtLevel = static_cast<LogLevel>(levelInt);
+   try {
+        defualtLevel = toLogLevel(std::stoi(argv[2]));
    }
+   catch(const std::exception& e) {
+        std::cerr << "Error: " << e.what() <<std::endl;
+        return 1;
+   }
+
+  
    
    try {
-        //реализация проверки работы логера
+        Logger logger { logfile, defualtLevel };
+        std::cout << "Logger initialization. Logging file: " << logfile 
+                  << ". Defualt logging level: " << static_cast<int>(defualtLevel) << std::endl;
+        
+        ThreadSafeQueue<LogTask> queue;
+        std::thread worker(workerThread, std::ref(queue), std::ref(logger));
+
+        while ( true ) {
+
+            std::string input_str;
+            std::cout << "Enter message (Enter 'exit' to exit):" << std::endl;
+            std::getline(std::cin, input_str);
+            
+            if (input_str == "exit") break;
+
+            std::string level_str;
+            std::cout << "Enter level (defualt - " << static_cast<int>(defualtLevel) << "):" << std::endl;
+            std::getline(std::cin, level_str);
+            
+            LogLevel level { defualtLevel };
+            try {
+                level = toLogLevel(std::stoi(level_str));
+            }
+            catch(...) {
+                std::cerr << "Invalid level. Use defualt!" << std::endl;
+            }  
+            
+            queue.push( {input_str, level} );
+        }
+
+        queue.stop();
+        worker.join();
+        std::cout << "Aplicated stopped" << std::endl;
+                          
    } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() <<std::endl;
         return 1;
    }
 
 
-   system("PAUSE");
+   std::cin.get();
 
    return 0;
 }
