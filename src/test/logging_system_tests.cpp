e7d2e0d83ea63de62096de5b1f1ebfd5a6d5bed7
Latypov <ruslanlatypov14@gmail.com>
2025-08-18 16:49:12 +0500

    Создание файла с автотестами + добавление нового таргета для сборки и автозапуска тестов

---
+++ src/test/logging_system_tests.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int main () {
+    std::cout << "Starting auto logger tests..." << std::endl;
+
+    //реализовать авто тесты
+
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
+}
